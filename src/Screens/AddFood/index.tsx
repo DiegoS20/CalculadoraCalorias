b297e8e3020d79dd6f0ba1122c219e49e3e859c4
Diego Saravia <saravia__2000@hotmail.com>
2024-01-13 19:39:10 -0600

    feat: AddFood search section

--- src/Screens/AddFood/index.tsx
+++ src/Screens/AddFood/index.tsx
@@ -1,12 +1,36 @@
 import {Text, View} from 'react-native';
+import {Button, Icon, Input} from '@rneui/themed';
 import Header from '../../components/Header';
 
 import styles from './styles';
 
 export default function AddFood() {
   return (
-    <View>
+    <View style={styles.screen}>
       <Header />
+      <View style={styles['add-food-container']}>
+        <View style={styles['legend-container']}>
+          <Text style={{fontSize: 20}}>Add Food</Text>
+        </View>
+        <View style={styles['add-food-btn-container']}>
+          <Button
+            icon={<Icon name="add-circle-outline" color="#fff" />}
+            radius="lg"
+            color="#4ecb71"
+          />
+        </View>
+      </View>
+      <View style={styles['search-container']}>
+        <View style={styles['input-container']}>
+          <Input placeholder="Apples, Fries, Soda..." />
+        </View>
+        <Button
+          title="Search"
+          color="#ade8af"
+          titleStyle={styles['search-btn-title-styles']}
+          radius="lg"
+        />
+      </View>
     </View>
   );
 }
--- src/Screens/AddFood/styles.ts
+++ src/Screens/AddFood/styles.ts
@@ -1,3 +1,30 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  ['screen']: {
+    padding: 12,
+  },
+  ['add-food-container']: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 24,
+  },
+  ['legend-container']: {
+    flex: 1,
+  },
+  ['add-food-btn-container']: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  ['search-container']: {
+    flexDirection: 'row',
+  },
+  ['input-container']: {
+    flex: 1,
+    marginLeft: -12,
+  },
+  ['search-btn-title-styles']: {
+    color: '#000',
+    fontSize: 14,
+  },
+});
