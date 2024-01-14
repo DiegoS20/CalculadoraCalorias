bf597dd7841eae9f926a86956f266d7a6bb1b299
Diego Saravia <saravia__2000@hotmail.com>
2024-01-13 19:07:23 -0600

    feat: initial AddFood screen

---
+++ src/Screens/AddFood/index.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import Header from '../../components/Header';
+
+import styles from './styles';
+
+export default function AddFood() {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+}
