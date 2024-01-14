d3e8573ad261850821faed41f7a0a1601120ffa7
Diego Saravia <saravia__2000@hotmail.com>
2024-01-13 19:06:11 -0600

    feat: typing navigation instance and adding AddFood screen

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,25 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './Screens/Home';
+import HomeScreen from './screens/Home';
+import AddFood from './screens/AddFood';
 
-const {Navigator, Screen} = createNativeStackNavigator();
+const {Navigator, Screen} = createNativeStackNavigator<RootStackParamList>();
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator>
+      <Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Screen name="Home" component={HomeScreen} />
+        <Screen name="AddFood" component={AddFood} />
       </Navigator>
     </NavigationContainer>
   );
 }
+
+export type RootStackParamList = {
+  Home: undefined;
+  AddFood: undefined;
+};
